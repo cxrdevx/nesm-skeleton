d87cca83cafda9cd8618ae381cf6b4301d9a2948
Carlos Tapia <carlostapiaolguin1@gmail.com>
2018-07-09 18:13:26 -0400

    se agregar metodos CRUD faltantes

--- node/app/controllers/todos.js
+++ node/app/controllers/todos.js
@@ -22,14 +22,52 @@ class TodosController {
 
     async getTodo(req,res) {
         try{
-            var todo = await model.Todo.find({ where: { id: req.params.id } })
+            var todo = await model.Todo.find({ where: { id: req.params.id } });
             if (todo) {
-                res.status(200).json(todo)
+                res.status(200).json(todo);
             } else {
-                res.status(204).send("No Content")
+                res.status(204).send("No Content");
             }
         } catch(ex) {
-            res.status(500).send({ message: ex.toString() })
+            res.status(500).send({ message: ex.toString() });
+        }
+    }
+
+    async createTodo(req, res){
+        try{
+            var todo = await model.Todo.create({
+                title: req.body.title,
+                description: req.body.description
+            });
+            res.status(200).send(todo);
+        } catch(ex){
+            res.status(500).send({ message: ex.toString() });
+        }
+    }
+
+    async updateTodo(req, res){
+        try{
+            var todo = await model.Todo.find({where : {id : req.params.id}});
+            if (!todo) {
+                throw new Error("Todo buscado no existe")
+            }
+            var updateTodo = await model.Todo.update(
+                { title: req.body.title,
+                    description: req.body.description}
+                ,{returning: true, where: {id: req.params.id}
+            });
+            res.status(200).send(updateTodo);
+        } catch(ex){
+            res.status(500).send({ message: ex.toString() });
+        }
+    }
+
+    async deleteTodo(req, res){
+        try{
+            await model.Todo.destroy({ where: { id: req.params.id } });
+            res.status(200).send("deleted");
+        } catch(ex){
+            res.status(500).send({ message: ex.toString() });
         }
     }
 }
--- node/app/routes/todos.js
+++ node/app/routes/todos.js
@@ -2,43 +2,12 @@ var express = require('express');
 var router = express.Router();
 var model = require('../models/index');
 
-var todosController = require('../controllers/todos')
+var todosController = require('../controllers/todos');
  
-/* GET todo listing. */
 router.get('/', todosController.getTodos);
 router.get('/:id', todosController.getTodo);
-/* POST todo. */
-router.post('/', function(req, res, next) {
-    const {
-        title,
-        description
-    } = req.body;
-    model.Todo.create({
-            title: title,
-            description: description
-        })
-        .then(todo => res.status(201).json({
-            error: false,
-            data: todo,
-            message: 'New todo has been created.'
-        }))
-        .catch(error => res.json({
-            error: true,
-            data: [],
-            error: error
-        }));
-});
- 
- 
-/* update todo. */
-router.put('/:id', function(req, res, next) {
- 
-});
- 
- 
-/* GET todo listing. */
-router.delete('/:id', function(req, res, next) {
- 
-});
+router.post('/', todosController.createTodo);
+router.put('/:id', todosController.updateTodo);
+router.delete('/:id', todosController.deleteTodo);
  
 module.exports = router;
